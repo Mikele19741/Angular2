1dafb408391b29b8c0eb39687a99a12ea42e97b3
MikeShilkov <mikhails@holistics.ru>
2019-09-09 12:16:37 +0300

    components parameters

--- Angular2Test/src/app/car/car.component.ts
+++ Angular2Test/src/app/car/car.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+
+import { Component, Input } from '@angular/core';
 
 @Component({
   selector: 'app-car',
@@ -7,10 +8,7 @@ import { Component, OnInit } from '@angular/core';
 })
 export class CarComponent {
 
-carName: string ="Ford";
-carYear: string="2015"
-getName(){
-return this.carName;
-}
+@Input('carItem') car: {name: string, year: number}
 
+private carItem=false;
 }
--- Angular2Test/src/app/cars/cars.component.ts
+++ Angular2Test/src/app/cars/cars.component.ts
@@ -10,8 +10,11 @@ export class CarsComponent {
 
  inputText='';
  carName='';
+ carYear=2017;
   addCarsStatus=false;
-  cars = ["Ford", "Audi", "BMV", "Wolkswagen"]; 
+  cars = 
+  [{name:"Ford",
+    year:2015},{name:"Lada", year:2018}]; 
   items=[{id: 4, name:'11'},{id: 2, name:'22'},{id: 3, name:'33'}]
   dates=[ new Date(2015, 3,4), new Date(2011, 1,4), new Date(2013, 3,3), new Date(2016, 3,5)];
  constructor(){
@@ -20,8 +23,9 @@ export class CarsComponent {
    addCar()
    {
      this.addCarsStatus=true;
-     this.cars.push(this.carName);
+     this.cars.push({name: this.carName, year: this.carYear});
      this.carName='';
+     this.carYear=2017;
    }
    onKeyUp(value)
    {
