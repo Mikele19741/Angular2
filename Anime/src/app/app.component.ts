34fc0ddcbf4773dfbe5930553a7d25887eb9b35e
MikeShilkov <mikhails@holistics.ru>
2019-10-29 12:19:50 +0300

    Animations with trigger

--- Anime/src/app/app.component.ts
+++ Anime/src/app/app.component.ts
@@ -1,10 +1,20 @@
 import { Component } from '@angular/core';
+import { trigger, style, state, transition, animate } from '@angular/animations';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
+  animations:[
+    trigger('clickedDiv',[state('start',style({backgroundColor:'blue', width: '150px', height:'150px'})),
+     state('end', style({backgroundColor:'red', width: '300px', height:'300px'})), transition('start => end', animate(1500)), transition('end=>start',animate('800ms 500ms ease-out'))])
+  ]
 })
 export class AppComponent {
-  title = 'Anime';
+  clickedDivState='start';
+  changeDivState()
+  {
+    this.clickedDivState='end';
+    setTimeout(()=>{this.clickedDivState='start';},3000);
+  }
 }
