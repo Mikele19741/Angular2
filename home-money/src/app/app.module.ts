5d14ec19d307eecee2d6ae1807f8debc27ad2fb6
MikeShilkov <mikhails@holistics.ru>
2019-11-08 15:26:14 +0300

    validate password

--- home-money/src/app/app.module.ts
+++ home-money/src/app/app.module.ts
@@ -4,6 +4,7 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { AuthModule } from './auth/auth.module';
+import { SharedModule } from './shared/shared.module';
 
 @NgModule({
   declarations: [
@@ -13,6 +14,7 @@ import { AuthModule } from './auth/auth.module';
     BrowserModule,
     AppRoutingModule,
     AuthModule
+  
   ],
   providers: [],
   bootstrap: [AppComponent]
--- home-money/src/app/auth/auth.module.ts
+++ home-money/src/app/auth/auth.module.ts
@@ -1,18 +1,22 @@
 import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
 import { LoginComponent } from './login/login.component';
 import { RegistrationComponent } from './registration/registration.component';
 import { AuthComponent } from './auth.component';
-import { CommonModule } from '@angular/common';
 import { AuthRoutingModule } from './auth-routing.module';
+import { SharedModule } from '../shared/shared.module';
 
 @NgModule({
-  declarations:[
-      LoginComponent,
-      RegistrationComponent,
-      AuthComponent
+  declarations: [
+    LoginComponent,
+    RegistrationComponent,
+    AuthComponent
   ],
-  imports: [CommonModule, AuthRoutingModule]
+  imports: [
+    CommonModule,
+    AuthRoutingModule,
+    SharedModule
+  ]
 })
-export class AuthModule{
-
-}
+export class AuthModule {}
