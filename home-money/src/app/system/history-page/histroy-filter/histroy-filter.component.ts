40e69cedd0111ae2759a0a2a3dc4f88981afe08e
MikeShilkov <mikhails@holistics.ru>
2019-12-26 18:13:46 +0300

    added filter

--- home-money/src/app/system/history-page/histroy-filter/histroy-filter.component.ts
+++ home-money/src/app/system/history-page/histroy-filter/histroy-filter.component.ts
@@ -1,15 +1,62 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, EventEmitter, Output, Input } from '@angular/core';
+import { Category } from '../../shared/models/category.model';
 
 @Component({
   selector: 'wfm-histroy-filter',
   templateUrl: './histroy-filter.component.html',
   styleUrls: ['./histroy-filter.component.scss']
 })
-export class HistroyFilterComponent implements OnInit {
+export class HistroyFilterComponent {
 
-  constructor() { }
+  @Output() onFilterCancel = new EventEmitter<any>();
+  @Output() onFilterApply = new EventEmitter<any>();
 
-  ngOnInit() {
+  @Input() categories: Category[] = [];
+
+  selectedPeriod = 'd';
+  selectedTypes = [];
+  selectedCategories = [];
+
+  timePeriods = [
+    {type: 'd', label: 'День'},
+    {type: 'w', label: 'Неделя'},
+    {type: 'M', label: 'Месяц'}
+  ];
+
+  types = [
+    {type: 'income', label: 'Доход'},
+    {type: 'outcome', label: 'Расход'}
+  ];
+
+  closeFilter() {
+    this.selectedTypes = [];
+    this.selectedCategories = [];
+    this.selectedPeriod = 'd';
+    this.onFilterCancel.emit();
+  }
+
+  private calculateInputParams(field: string, checked: boolean, value: string) {
+    if (checked) {
+      this[field].indexOf(value) === -1 ? this[field].push(value) : null;
+    } else {
+      this[field] = this[field].filter(i => i !== value);
+    }
+  }
+
+  handleChangeType({checked, value}) {
+    this.calculateInputParams('selectedTypes', checked, value);
+  }
+
+  handleChangeCategory({checked, value}) {
+    this.calculateInputParams('selectedCategories', checked, value);
+  }
+
+  applyFilter() {
+    this.onFilterApply.emit({
+      types: this.selectedTypes,
+      categories: this.selectedCategories,
+      period: this.selectedPeriod
+    });
   }
 
 }
